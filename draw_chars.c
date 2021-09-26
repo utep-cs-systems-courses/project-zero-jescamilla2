795460969ba018feb3917bcd0e02f84ba4be718e
Jeffrey <jescamilla2@miners.utep.edu>
2021-09-25 21:52:29 -0600

    updated 8x12-font to print correctly

--- draw_chars.c
+++ draw_chars.c
@@ -12,16 +12,16 @@
     }
     putchar('\n');
   }
-}
-*/
+} */
+
 
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 8; row++) {
-    unsigned short rowBits = font_8x12[c][row];
-    for (char col = 0; col < 12; col++) {
-      unsigned short colMask = 1 << (11-col); // mask to select bit associated with bit 
+  for (char col = 0; col < 8; col++) {
+    for (char row = 12; row >= 0; row--) {
+      unsigned short rowBits = font_8x12[c][row];
+      unsigned short colMask = 1 << (7-col); // mask to select bit associated with bit 
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
